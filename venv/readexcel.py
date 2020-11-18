5df253a3a75f9214da39e8bcc8adb4432334a18d
yurkee <704573039@qq.com>
2020-11-19 00:10:13 +0800

    遍历整个Excel超链接

--- venv/readexcel.py
+++ venv/readexcel.py
@@ -6,11 +6,20 @@ def readexcel():
 
     # #通过名称打开表单
     mySheet = rdexcel.active
-    # # #直接获取某行某列单元格数据
-    myCellValues = mySheet.cell(row=59, column=3).hyperlink.target
+    #直接获取某行某列单元格数据
+    # myCellValues = mySheet.cell(row=59, column=3).hyperlink.target
+
+    for row in mySheet.iter_rows():
+        for cell in row:
+            if cell.value != None:
+                myLink = cell.hyperlink.target[22:]
+        print(myLink)
+
+    # mycell = mySheet.cell(row=1, column=1).hyperlink.target
+    # print(mycell)
 
     #获取单元格超链接
 
-    print(myCellValues)
+    # print(myCellValues)
 
 readexcel()
