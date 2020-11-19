2bf114e38d5819b7929ca2642da060fecfada5c4
sicong <weisicong0619@dingtalk.com>
2020-11-19 18:32:38 +0800

    超链接地址用列表保存

--- venv/readexcel.py
+++ venv/readexcel.py
@@ -1,25 +1,24 @@
+# coding=utf-8
 import openpyxl
 
 def readexcel():
     #打开文件
-    rdexcel = openpyxl.load_workbook('/Users/yurkee/Desktop/seo-v1-43-0/sitemap.xlsx')
+    rdexcel = openpyxl.load_workbook('D:\work\seo-v1-43-0\sitemap.xlsx')
 
     # #通过名称打开表单
     mySheet = rdexcel.active
     #直接获取某行某列单元格数据
     # myCellValues = mySheet.cell(row=59, column=3).hyperlink.target
 
+    myList = []
     for row in mySheet.iter_rows():
         for cell in row:
             if cell.value != None:
                 myLink = cell.hyperlink.target[22:]
-        print(myLink)
+                myList.append(myLink)
+    print(myList)
 
     # mycell = mySheet.cell(row=1, column=1).hyperlink.target
     # print(mycell)
-
-    #获取单元格超链接
-
-    # print(myCellValues)
 
 readexcel()
