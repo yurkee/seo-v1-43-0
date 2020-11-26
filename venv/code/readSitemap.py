1b07ee52aff31568f9abecd55b59a42a0f26c1a1
sicong <weisicong0619@dingtalk.com>
2020-11-26 15:13:33 +0800

    完善代码注释

--- venv/code/readSitemap.py
+++ venv/code/readSitemap.py
@@ -4,22 +4,31 @@ import urllib.request
 import ssl
 import re
 
+#解决访问https时不受信任SSL证书问题
 ssl._create_default_https_context = ssl._create_unverified_context
 
 def get_web_link_value():
+    #打开URL地址，读取页面
     page = urllib.request.urlopen("https://v1-43-0.btclass.net/sitemap")
     contents = page.read()
 
+    #将html解析存入定义的soup
     soup = BeautifulSoup(contents, "html.parser")
 
+    # #查找符合<a>标签中含有class属性值为"site-link"的所有标签，并以列表形式保存
     # linkstr = soup.find_all('a', class_="site-link")
     # print(linkstr)
+
+    #用字典的方式存储，从页面中读取的值和超链接
     stDict = {}
     for i in soup.find_all('a', class_='site-link'):
         # print(i)
         # res = re.search(r'\<(.*?)\>', i)
         # print(res)
+
+        #获取标签<a>中，href的属性值
         myLink = i['href']
+        #获取标签<a>的text
         myText = i.string
         Dict = {myText:myLink}
         stDict.update(Dict)
--- venv/code/resultcompare.py
+++ venv/code/resultcompare.py
@@ -2,11 +2,14 @@ import readexcel
 import readSitemap
 import csv
 
+#实例化方法
 rc = readexcel.read_Excel()
 rs = readSitemap.get_web_link_value()
 
+#两字典对比，保存对比结果不同的差异处
 differ = set(rc.items()) ^ set(rs.items())
 
+#写入指定文件
 f = open('D:\work\seo-v1-43-0\\result.csv', 'w', encoding='utf-8')
 csv_writer = csv.writer(f)
 csv_writer.writerow(differ)
